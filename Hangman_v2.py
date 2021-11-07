932d4cb6965d067a1d283375afb61b9b3f69d04d
Paola <paola.lopmat@gmail.com>
2021-11-07 09:57:05 -0600

    Prueba ahorcado

--- Hangman_v2.py
+++ Hangman_v2.py
@@ -1,25 +1,64 @@
 # Valores iniciales
+# Valores iniciales
 from numpy import random
 
-reinicio = 's'
-while reinicio == 's':
-
+reiniciar = 's'
+while reiniciar == 's':
     lista_de_palabras = ['alfabeto','noviembre','ironhack','murcielago','ferrocarril']
     letras_anteriores = []
     letras_adivinadas =[]
     letra_actual = []
-    vidas = 3
+    vidas = 7
     palabra = random.choice(lista_de_palabras)
     letras_palabra = []
 
+
+
     for letra in palabra:
         letras_palabra.append(letra)
+
+#     def reiniciar():
+#         jugar_otravez = []
+#         jugar_otravez = input('Reiniciar juego s = Si, n = No')
+
+#         while jugar_otravez not in ['s', 'n']:
+#             jugar_otravez = input('Reiniciar juego s = Si, n = No')
+
+#         if jugar_otravez == "s":
+#             palabra = random.choice(lista_de_palabras)
+#             vidas = 3
+#             for letra in palabra:
+#                 letras_palabra.append(letra)
+
+#             while vidas > 0:
+#                 letra_actual = pedir_letra()
+
+#                 letras_adivinadas, vidas = acertar_letra(letra_actual, vidas)
+
+#                 print(esconder_palabra(letras_adivinadas),' Te quedan',vidas,'vidas')
+
+#                 if set(letras_adivinadas) == set(letras_palabra):
+#                     print('\nGANASTEEEE!\n')
+#                     break
+
+#                 if vidas == 0:
+#                     print('\nPerdiste :( \n')
+#                     break
+
+#             reiniciar()
+
+#         elif jugar_otravez == "n":
+#             print("Gracias por jugar")
+#             exit()
+#         else:
+#             jugar_otravez = input('Respuesta incorrecta. Reiniciar juego s = Si, n = No')     
+
     def pedir_letra():
         # guardar la letra que nos de el usuario
         letra_actual = input('Dame una letra ').lower()
         while letra_actual in letras_anteriores:
             #repite
-            letra_actual = input('Dame otra letra ').lower()
+            letra_actual = input('Dame otra letra').lower()
 
         letras_anteriores.append(letra_actual)        
         return letra_actual
@@ -31,9 +70,36 @@ while reinicio == 's':
             return letras_adivinadas, vidas
         else:
             vidas = vidas - 1
-            return letras_adivinadas, vidas
-
-
+            if vidas == 6:
+                print("  _____ \n"
+                      "  |      \n"
+                      "  |      \n"
+                      "  |      \n"
+                      "  |      \n"
+                      "  |      \n"
+                      "  |      \n"
+                      "__|__\n")
+                print("Letra erronea. " + str(vidas) + " intentos restantes\n")
+            elif vidas == 5:
+                print("_____ \n"
+                  "  |     | \n"
+                  "  |     |\n"
+                  "  |     | \n"
+                  "  |     O \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "__|__\n")
+                print("Letra erronea. " + str(vidas) + " intentos restantes\n")
+            elif vidas == 4:
+                print("  _____ \n"
+                      "  |    | \n"
+                      "  |    |\n"
+                      "  |    | \n"
+                      "  |    O \n"
+                      "  |   / \n"
+                      "__|__\n")
+                print("Letra erronea. " + str(vidas) + " intentos restantes\n")
+        return letras_adivinadas, vidas
 
     def esconder_palabra(letras_adivinadas): # 'palabra' ---> '_ _ _ _ _ _ _'
 
@@ -42,12 +108,10 @@ while reinicio == 's':
             if letra not in letras_adivinadas:
                 letra = '_ '
                 letra2.append(letra)
-        #         print(letra)
             else:
                 letra2.append(letra+' ')
-        #         print(letra2)
 
-        return ''.join(letra2)
+        return ''.join(letra2)    
 
     while vidas > 0:
         letra_actual = pedir_letra()
@@ -57,9 +121,18 @@ while reinicio == 's':
         print(esconder_palabra(letras_adivinadas),' Te quedan',vidas,'vidas')
 
         if set(letras_adivinadas) == set(letras_palabra):
-            print('\nGANASTEEEE!')
+            print('\nGANASTEEEE!\n')
             break
+
     if vidas == 0:
-        print('\nPerdiste :( ')
+        print('\nPerdiste :( \n')
         
-    reinicio = input('Deseas reiniciar? s = sí, n = no ')
+    reiniciar = input('Reiniciar juego s = Si, n = No')
+    
+# letras_anteriores = []
+# letras_adivinadas = []
+# letra_actual = []
+# vidas += 3
+# letras_palabra = []
+# palabra = random.choice(lista_de_palabras)
+# # reiniciar()
